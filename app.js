8dba10fa0f16de673eac6026e3c6c2dbac79d23f
reggistrac <reggistrac@yandex.ru>
2021-04-25 22:42:49 +0300

    Третья проверка

--- app.js
+++ app.js
@@ -7,30 +7,24 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 mongoose.connect('mongodb://localhost:27017/mestodb', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
 	useFindAndModify: false,
 });
 
-const routeses = ['/users', '/cards'];
 app.use((req, res, next) => {
-	let f = false;
-	for (let i = 0; i < routeses.length; i++) {
-		if (req.url.startsWith(routeses[i])) {
-			f = true;
-			break;
-		}
-	}
-	if (f) {
 		req.user = { _id: '6081a2ef55692028401f3826' };
 		next();
-	}
-	else {
-		res.status(404).send({ message: 'Страница не найдена' });
-	}
 });
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use('/', (req, res) => {
+	res.status(404).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT);
